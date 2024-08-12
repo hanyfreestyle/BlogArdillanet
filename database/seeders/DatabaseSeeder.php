6e928a4b1df630c0b0e79b1755fe458e279a1d61
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-12 08:55:49 +0300

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -72,8 +72,8 @@ class DatabaseSeeder extends Seeder {
             if (File::isFile(base_path('routes/AppPlugin/blogPost.php'))) {
                 $this->call(BlogCategorySeeder::class);
                 $this->call(BlogPostSeeder::class);
-                $this->call(BlogTranslationSeeder::class);
-                $this->call(BlogTranslationSeeder2::class);
+//                $this->call(BlogTranslationSeeder::class);
+//                $this->call(BlogTranslationSeeder2::class);
 //                $this->call(BlogTranslationSeeder3::class);
                 $this->call(BlogTranslationSeeder4::class);
             }
