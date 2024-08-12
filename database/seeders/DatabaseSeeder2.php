c2d092a796d23e8e2814afad6d319fecbff891f0
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-12 09:02:38 +0300

    Update

--- database/seeders/DatabaseSeeder2.php
+++ database/seeders/DatabaseSeeder2.php
@@ -1,15 +1,14 @@
 <?php
 
 namespace Database\Seeders;
-use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder;
-
+use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder2;
 use Illuminate\Database\Seeder;
 
 
 class DatabaseSeeder2 extends Seeder {
 
     public function run(): void {
-        $this->call(BlogTranslationSeeder::class);
+        $this->call(BlogTranslationSeeder2::class);
     }
 
 }
